6e2265c67874bdde35f0d42a7b79b2fc8d3e128d
Owuraku Zenas <awukuzenas@gmail.com>
2025-09-16 15:11:20 +0000

    Add is_underage method to Student class and update main.py to demonstrate its usage

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ class Student:
         print(f"Name :{self.age}")
         print(f"Name :{self.grade}")
 
+    def is_underage(self):
+        if self.age < 18:
+            return True
+        else:
+            return False
 
-      
-    
+
+student1 = Student("John",20,"A")
+student2 = Student("Jane",17,"B")
+
+print(student1.Display_info())
+print(student2.is_underage())
