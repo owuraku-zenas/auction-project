a324d0bb93613652d2a92c82f271e0b08e8367f0
Teteiba-Ange <aacquah041@st.ug.edu.gh>
2025-09-16 15:32:18 +0100

    changes

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print('Hello World')
+class Student:
+    def __init__(self,name,age,grade):
+        """Initializing a student object"""
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def Display_info(self):
+        print(f"Name :{self.name}")
+        print(f"Name :{self.age}")
+        print(f"Name :{self.grade}")
+
+
+      
+    
